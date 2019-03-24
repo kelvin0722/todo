3af239f49290348537b2ad4cf152829d40dcb998
kelvin0722 <kelvinm442@gmail.com>
2019-03-24 08:48:27 +0300

    todo app setup

---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-for="(item, index) in todos" :key="index">
+            <h1>{{item.title}}</h1>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todos',
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
